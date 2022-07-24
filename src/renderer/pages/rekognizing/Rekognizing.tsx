7cc0c026e37325cb997f19e803f58c9958ae80ce
Lautaro Garcia <lautarojgarcia177@gmail.com>
2022-07-24 01:02:50 -0300

    rekognition finish alert

--- src/renderer/pages/rekognizing/Rekognizing.tsx
+++ src/renderer/pages/rekognizing/Rekognizing.tsx
@@ -1,10 +1,11 @@
-import { Progress } from '@chakra-ui/react';
+import { Progress, useDisclosure } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { VStack } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import * as store from '../../store';
 import { useSelector } from 'react-redux';
+import FinishRekogntionAndTagAlert from 'renderer/components/finish-rekognition-and-tag-alert/FinishRekognitionAndTagAlert';
 
 type ProcessProgressProps = {
   title: string;
@@ -30,6 +31,7 @@ const Rekognizing = () => {
   const navigate = useNavigate();
   const [rekognitionProgress, setRekognitionProgress] = useState(0);
   const [exifToolTaggingProgress, setexifToolTaggingProgress] = useState(0);
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   useEffect(() => {
     window.electron.onRekognitionProgress((_event, progress) => {
@@ -39,6 +41,9 @@ const Rekognizing = () => {
     window.electron.onExifToolTagProgress((_event, progress) => {
       console.log('exiftool progress', progress);
       exifToolTaggingProgressHandler(progress);
+    });
+    window.electron.onExifToolTagFinish(() => {
+      handleExifToolTagFinish();
     });
     // return () => {
     // TODO Remove rekognition progress listener and exiftool tag progress listener
@@ -51,30 +56,40 @@ const Rekognizing = () => {
   function exifToolTaggingProgressHandler(progress: number): void {
     setexifToolTaggingProgress(progress);
   }
-
+  function handleExifToolTagFinish() {
+    onOpen();
+  }
   function handleCancel(): void {
     navigate('/');
   }
 
   return (
-    <VStack>
-      <ProcessProgress
-        title={
-          rekognitionProgress !== 100
-            ? 'Rekonociendo numeros con AWS Rekognition'
-            : 'Etiquetando las imagenes con Exiftool'
-        }
-        progressValue={
-          rekognitionProgress !== 100
-            ? rekognitionProgress
-            : exifToolTaggingProgress
-        }
+    <>
+      <VStack>
+        <ProcessProgress
+          title={
+            rekognitionProgress !== 100
+              ? 'Rekonociendo numeros con AWS Rekognition'
+              : 'Etiquetando las imagenes con Exiftool'
+          }
+          progressValue={
+            rekognitionProgress !== 100
+              ? rekognitionProgress
+              : exifToolTaggingProgress
+          }
+        />
+        <Button colorScheme="red" onClick={handleCancel}>
+          Cancel
+        </Button>
+        ;
+      </VStack>
+      {/* https://chakra-ui.com/docs/components/alert-dialog */}
+      <FinishRekogntionAndTagAlert
+        onOpen={onOpen}
+        onClose={onClose}
+        isOpen={isOpen}
       />
-      <Button colorScheme="red" onClick={handleCancel}>
-        Cancel
-      </Button>
-      ;
-    </VStack>
+    </>
   );
 };
 
